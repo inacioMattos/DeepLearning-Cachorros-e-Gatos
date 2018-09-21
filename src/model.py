859676ff98a5793172708624bced20bca915a5c3
Inácio Mattos <inaciotm@hotmail.com>
2018-09-21 19:42:15 -0300

    Updating the model and saving it

--- src/model.py
+++ src/model.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-import pickle, os
+from tensorflow.keras.callbacks import TensorBoard
+import pickle, os, time
 
 
 DATADIR="data/"
+NAME="cachorros-gatos-cnn-64x3-{}".format(int(time.time()))
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 
 
 def getData():
@@ -18,11 +22,16 @@ def normalizeData(X):
 	return X/255.0	# já que numa imagem o valor máximo é 255 para cada pixels, é só dividir por 255.
 
 
+def saveModel(model):
+	model.save("64*2x3-CNN.model")
+
+
 def trainModel(model, training_set):
 	X, y = training_set
 
 	model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-	model.fit(X, y, batch_size=32, validation_split=0.1, epochs=3)
+	model.fit(X, y, batch_size=32, validation_split=0.1, epochs=7, callbacks=[tensorboard])
+	return model
 
 
 def createModel(X):
@@ -32,16 +41,15 @@ def createModel(X):
 	model.add(Activation("relu"))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 
-	model.add(Conv2D(64, (3,3)))
+	model.add(Conv2D(128, (4,4)))
 	model.add(Activation("relu"))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
-	'''
-	model.add(Conv2D(64, (3,3)))
+	
+	model.add(Conv2D(256, (3,3)))
 	model.add(Activation("relu"))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
-	'''
+	
 	model.add(Flatten())
-	model.add(Dense(64))
 
 	model.add(Dense(1))
 	model.add(Activation("sigmoid"))
@@ -53,7 +61,8 @@ def main():
 	X, y = getData()
 	X = normalizeData(X)
 	model = createModel(X)
-	trainModel(model, (X, y))
+	model = trainModel(model, (X, y))
+	saveModel(model)
 
 
 
